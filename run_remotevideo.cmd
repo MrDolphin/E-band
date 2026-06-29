902e157b492cb9f7e969a8cf64695cf0bf598da9
MrDolphin <573884287@qq.com>
2026-06-29 16:53:05 +0800

    Point launcher to unified repair build

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-ec9a54f\Release\net8.0-windows7.0"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-a2a6452\Release\net8.0-windows7.0"
 start "" "%APP_DIR%\remotevideo.exe"
