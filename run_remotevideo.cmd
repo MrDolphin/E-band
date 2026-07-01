b0cd5bf99a081133be628470e4f9b59c7f4b896c
MrDolphin <573884287@qq.com>
2026-07-01 09:39:47 +0800

    Point launcher to real-time video build

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-priority\Release\net8.0-windows7.0"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-realtime-488d5fa\Release\net8.0-windows7.0"
 start "" "%APP_DIR%\remotevideo.exe"
