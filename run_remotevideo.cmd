fcbec4f1b8c7846ba4126df34fc9d0115ccb3f50
MrDolphin <573884287@qq.com>
2026-07-01 23:43:38 +0800

    Point launcher to repair variant build

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-earliest-qpsk-7cd1a3b\Release\net8.0-windows7.0"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-variants-b554211\Release\net8.0-windows7.0"
 start "" "%APP_DIR%\remotevideo.exe"
