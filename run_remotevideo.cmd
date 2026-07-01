21d6a3530100b5921625bf2438443796803494c7
MrDolphin <573884287@qq.com>
2026-07-01 14:38:00 +0800

    Point launcher to RX continuity build

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-realtime-fcbac62\Release\net8.0-windows7.0"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-rx-continuity-ec91d3b\Release\net8.0-windows7.0"
 start "" "%APP_DIR%\remotevideo.exe"
