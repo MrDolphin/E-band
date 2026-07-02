96b6a38564a0454ae77f3ad4e56c8bfd22e91427
MrDolphin <573884287@qq.com>
2026-07-02 17:19:45 +0800

    Commit: Restore E310 burst gating default parameters (100 ADC threshold, 2U hangover frames) to clear noise storm

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-variants-bf74193\Debug\net8.0-windows7.0"
-start "" "%APP_DIR%\remotevideo-bf74193.exe"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-variants-ba39570\Debug\net8.0-windows7.0"
+start "" "%APP_DIR%\remotevideo-ba39570.exe"
