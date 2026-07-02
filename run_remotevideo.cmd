4efffadd2f85d86aa308b8d7685fb48c71f7c77b
MrDolphin <573884287@qq.com>
2026-07-02 17:25:35 +0800

    Commit: Adjust AD9361 TX attenuation to -20.0dB and RX gain to 35.0dB to optimize link budget

--- run_remotevideo.cmd
+++ run_remotevideo.cmd
@@ -1,3 +1,3 @@
 @echo off
-set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-variants-96b6a38\Debug\net8.0-windows7.0"
-start "" "%APP_DIR%\remotevideo-96b6a38.exe"
+set "APP_DIR=%~dp0Material\videotool_decompiled\remotevideo_usepdb\bin\video-modem-stage14-repair-variants-5876296\Debug\net8.0-windows7.0"
+start "" "%APP_DIR%\remotevideo-5876296.exe"
